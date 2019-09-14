a847a9183a441ebf0789a702958c461396e839c1
Erica <ericabasak@gmail.com>
2019-09-14 22:45:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,10 +4,10 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute1, value|
     value.each do |attribute2, value1|
       value1.each do |elem|
-        if result[elem] is nil
+        if result[elem].nil?
           result[elem] = {}
         end
-        if result[elem][attribute1] is nil
+        if result[elem][attribute1].nil?
       end
     end
    end  
