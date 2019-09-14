da010ce4a0c0ea7c91f4d27f48c4a50e28b205ee
Erica <ericabasak@gmail.com>
2019-09-14 22:56:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,7 +10,7 @@ def nyc_pigeon_organizer(data)
         if result[elem][attribute1].nil?
           result[elem][attribute1] = []
         end
-        result[elem][attribute1].push
+        result[elem][attribute1].push(attribute2)
       end
     end
    end  
