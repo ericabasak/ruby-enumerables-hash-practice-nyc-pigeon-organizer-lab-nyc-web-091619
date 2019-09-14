70053cca6960b870a8d736a7680a450f363d0c70
Erica <ericabasak@gmail.com>
2019-09-14 21:58:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,13 @@
 def nyc_pigeon_organizer(data)
-  Hash.new
   result = {}
   
   data.each do |key, value|
     value.each do |key1, value1|
       value1.each do |elem|
-      reult[elem = ]
-      
-  puts
+        result[elem] = {}
+      end
+    end
+   end  
+   return result
 end
 
